66f8ecadfd010a8f9d1f14b130b9c4b20dbe7da8
Sergey Dobrovolsky <misterdobrovolsky2017@gmail.com>
2023-08-08 18:31:12 +0300

    update

--- script.js
+++ script.js
@@ -1,24 +1,36 @@
 const docGet = (id) => document.getElementById(id);
 
 const spin = docGet('spin');
-const container = document.querySelector('.container');
-let number = Math.ceil(Math.random() * 10) * 45;
+const container = docGet('container');
+let degreeRotationCircle = Math.ceil(Math.random() * 40) * 45;
 let currentPlayer = 1;
-
+const circleRotationTime = 3000;
 
 spin.addEventListener('click', function () {
-   container.style.transform = 'rotate(-' + number + 'deg)';
-   let circle = container.style.transform
-   number += Math.ceil(Math.random() * 10) * 45;
-
+   container.style.transform = 'rotate(-' + degreeRotationCircle + 'deg)';
+   const circle = container.style.transform;
+   degreeRotationCircle += Math.ceil(Math.random() * 10) * 45;
+   disabledSpin()
    const currentPlayerElement = docGet(`player${currentPlayer}`);
    setTimeout(function () {
       currentPlayerElement.innerText = `Игрок ${currentPlayer}: ${CalculateNumberOnCircle(circle)}`
       currentPlayer++;
-      currentPlayer > 3 ? currentPlayer = 1 : currentPlayer;
-   }, 2000);
+      setTimeout(function () {
+         if (currentPlayer > 3) {
+            alert(`Игрок ${calculateWinner()} победил!`)
+            location.reload();
+         }
+      }, 500)
+   }, circleRotationTime);
 });
 
+// Блокируем кнопку Spin после нажатия, убираем блокировку после остановки колеса
+const disabledSpin = () => {
+   spin.disabled = true;
+   setTimeout(function () {
+      spin.disabled = false;
+   }, circleRotationTime)
+}
 
 // Вычесляем число на барабане
 const CalculateNumberOnCircle = (num) => {
@@ -28,4 +40,11 @@ const CalculateNumberOnCircle = (num) => {
       result -= 8
    }
    return result
+}
+
+// Определение победителя
+const calculateWinner = () => {
+   const transformString = (i) => +docGet(i).textContent.slice(-1);
+   const arrResults = [transformString('player1'), transformString('player2'), transformString('player3')];
+   return arrResults.indexOf(Math.max(...arrResults)) + 1
 }
